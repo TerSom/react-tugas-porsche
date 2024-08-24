70d30c94f83e3e318acffd7fcf9ef543442b1746
unknown <ibnua0291@gmail.com>
2024-08-24 15:38:24 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Article from './component/article/article'
+import Section from './component/article/Section'
+import Sosial from './component/article/Sosial'
+import Footer from './component/footer/footer'
+import Navbar from './component/Navbar/Navbar'
+import Discover from './component/article/Discover'
+import FooterCopyright from './component/footer/FooterCopyright'
+
+function App() {
+
+  return (
+    <>
+    <Navbar></Navbar>
+    <Article></Article>
+    <Section></Section>
+    <Sosial></Sosial>
+    <Discover></Discover>
+    <Footer></Footer>
+    <FooterCopyright></FooterCopyright>
+    </>
+  )
+}
+
+export default App
